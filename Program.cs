d75288960a37041917e9d0da2d5023adedbdbb8b
megabro <lakoongames@gmail.com>
2021-07-02 16:11:59 +0300

    added shit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Playlist_Saver
+{
+    class Program
+    {
+        private static void Usage()
+        {
+            Console.WriteLine("Usage: playlist_saver [--html] [--ytAPI] <html file>");
+        }
+        
+        //pl_saver 
+        static void Main(string[] args)
+        {
+            if (args.Length is < 1 or > 2)
+            {
+                Usage();
+                return;
+            }
+
+            switch (args[0])
+            {
+                case "--html":
+                    //HTML impl
+                    break;
+                case "--ytAPI":
+                    //ytAPI impl
+                    break;
+                default:
+                    Usage();
+                    break;
+            }
+
+            //Output list to console
+            Console.WriteLine("test");
+        }
+    }
+}
